a5d7e428629bccdc46367ddbbaeb77393143270e
Uladzislau K <uladzislau.kuzmin@gmail.com>
2024-04-16 13:10:15 +0700

    Fix parent node is null console errors

--- src/ImageResize.js
+++ src/ImageResize.js
@@ -132,7 +132,7 @@ export default class ImageResize {
     };
 
     hideOverlay = () => {
-        if (!this.overlay) {
+        if (!this.overlay || !this.quill.root.parentNode) {
             return;
         }
 
